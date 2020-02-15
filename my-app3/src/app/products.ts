33869bc7381792c03f427bed6fdc0f6d659b4cc3
Dalelkhanova <you@example.com>
2020-02-15 18:02:53 +0600

    Init

---
+++ my-app3/src/app/products.ts
@@ -0,0 +1,80 @@
+export const products = [
+  {
+    routerLink:"https://www.aliexpress.com/item/32952551589.html?spm=a2g0o.productlist.0.0.60ee2ec7XDHrl2&algo_pvid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c&algo_expid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c-11&btsid=0be3764315817010946445542e8e5c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
+    imgUrl:"https://ae01.alicdn.com/kf/HTB10id4ONnaK1RjSZFBq6AW7VXaw/HUITAN-Luxury-Wedding-Anniversary-font-b-Ring-b-font-with-Pear-Shape-Huge-CZ-Prong-Setting.jpg_220x220xz.jpg_.webp",
+    name: 'HUITAN Luxury Ring',
+    price: 2.28,
+    description: 'copper,Rose Gold Plated',
+    rating: 4.7 
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/HTB1vH8xOMDqK1RjSZSyq6yxEVXaL/Huitan-2PC-Bridal-font-b-Ring-b-font-with-Round-Brilliant-Cubic-Zircon-Prong-Setting-Anniversary.jpg_220x220xz.jpg_.webp",
+    name: 'Huitan 2PC Bridal Ring',
+    price: 1.48,
+    description: 'Brass,925 Silver plated',
+    rating: 4.5
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/H2cec09eed5a14a2a8ae587900929a75dS/Xuping-Fashion-Ring-High-Quality-Charm-Design-Rings-jewelry-Promotion-Wedding-Gift-for-Women-14077.jpg_220x220xz.jpg_.webp",
+    name: 'Xuping Fashion Ring',
+    price: 8.24,
+    description: 'Brass,925 Silver plated',
+    rating: 4.9
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/HTB1S4TWXe6sK1RjSsrbq6xbDXXab/choucong-Vintage-Court-Ring-925-sterling-Silver-Princess-cut-AAAAA-cz-stone-Engagement-Wedding-band-Rings.jpg_220x220xz.jpg_.webp",
+    name: 'choucong Vintage Ring',
+    price: 14.31,
+    description: '925 sterling Silver',
+    rating: 4.8
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/H4e23e18a8ae3405a8c95d7388a9c2efef/2020-new-luxury-round-925-sterling-silver-wedding-ring-set-for-women-lady-anniversary-gift-jewelry.jpg_220x220xz.jpg_.webp",
+    name: 'Huitan moonso',
+    price: 31.98,
+    description: 'new 925 sterling silver',
+    rating: 5.0
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/H280acc5a2abc47ebb9c15d78b80344c0w/JPalace-Heart-Red-Murano-Glass-Ring-925-Sterling-Silver-Rings-for-Women-Stackable-Ring-Band-Silver.jpg_220x220xz.jpg_.webp",
+    name: 'JPalace Heart Ring',
+    price: 9.99,
+    description: 'Brass,925 Silver plated',
+    rating: 4.8
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/HTB1nx__lljTBKNjSZFDq6zVgVXa2/CWWZircons-Luxury-Wide-Circle-Women-Wedding-Rings-AAA-Cubic-Zirconia-Fashion-Geometric-Round-CZ-Bridal-Ring.jpg_220x220xz.jpg_.webp",
+    name: 'CWWZircons Luxury',
+    price: 6.86,
+    description: 'Wide Circle Cubic Zirconia',
+    rating: 4.8
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/HTB1vH8xOMDqK1RjSZSyq6yxEVXaL/Huitan-2PC-Bridal-font-b-Ring-b-font-with-Round-Brilliant-Cubic-Zircon-Prong-Setting-Anniversary.jpg_220x220xz.jpg_.webp",
+    name: 'Huitan 2PC Bridal Ring',
+    price: 1.48,
+    description: 'Brass,925 Silver plated',
+    rating: 4.5
+  },
+  {
+    imgUrl:"https://ae01.alicdn.com/kf/H30a965dea5a54c4a9d0359f5a4c332ef1/Modian-New-Style-Classic-Real-925-Sterling-silver-Square-Ring-Fashion-Wedding-Luxury-jewelry-Sparkling-CZ.jpg_220x220xz.jpg_.webp",
+    name: 'Modian Classic Real',
+    price: 10.01,
+    description: '925 Sterling silver Square',
+    rating: 4.7
+  },
+  {
+     imgUrl:"https://ae01.alicdn.com/kf/HTB1iTbKXH1YBuNjSszhq6AUsFXat/PATAYA-New-Arrivals-585-Rose-Gold-Double-Row-Micro-wax-Inlay-Natural-Zircon-Hollow-Rings-Women.jpg_220x220xz.jpg_.webp",
+    name: 'PATAYA New Arrivals',
+    price: 11.31,
+    description: '585 Rose Gold Zircon',
+    rating: 4.8
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
