f0c3e640147915b3038244fc73bc092d78ea523c
Dalelkhanova <you@example.com>
2020-02-22 13:50:34 +0600

    add

--- my-app3/src/app/products.ts
+++ my-app3/src/app/products.ts
@@ -1,6 +1,6 @@
 export const products = [
   {
-    routerLink:"https://www.aliexpress.com/item/32952551589.html?spm=a2g0o.productlist.0.0.60ee2ec7XDHrl2&algo_pvid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c&algo_expid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c-11&btsid=0be3764315817010946445542e8e5c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
+    url:"https://www.aliexpress.com/item/32953871001.html?spm=a2g0o.productlist.0.0.60ee2ec7XDHrl2&s=p&algo_pvid=85864b2f-cfe6-4784-af60-2071f70d4e70&algo_expid=85864b2f-cfe6-4784-af60-2071f70d4e70-4&btsid=0ab6fb8315816901736448843e0247&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/HTB10id4ONnaK1RjSZFBq6AW7VXaw/HUITAN-Luxury-Wedding-Anniversary-font-b-Ring-b-font-with-Pear-Shape-Huge-CZ-Prong-Setting.jpg_220x220xz.jpg_.webp",
     name: 'HUITAN Luxury Ring',
     price: 2.28,
@@ -8,6 +8,7 @@ export const products = [
     rating: 4.7 
   },
   {
+    url:"https://www.aliexpress.com/item/32952551589.html?spm=a2g0o.productlist.0.0.60ee2ec7XDHrl2&algo_pvid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c&algo_expid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c-11&btsid=0be3764315817010946445542e8e5c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/HTB1vH8xOMDqK1RjSZSyq6yxEVXaL/Huitan-2PC-Bridal-font-b-Ring-b-font-with-Round-Brilliant-Cubic-Zircon-Prong-Setting-Anniversary.jpg_220x220xz.jpg_.webp",
     name: 'Huitan 2PC Bridal Ring',
     price: 1.48,
@@ -15,6 +16,7 @@ export const products = [
     rating: 4.5
   },
   {
+    url:"https://www.aliexpress.com/item/32810725590.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7&algo_expid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7-31&btsid=0be3743b15822029777281371e805e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/H2cec09eed5a14a2a8ae587900929a75dS/Xuping-Fashion-Ring-High-Quality-Charm-Design-Rings-jewelry-Promotion-Wedding-Gift-for-Women-14077.jpg_220x220xz.jpg_.webp",
     name: 'Xuping Fashion Ring',
     price: 8.24,
@@ -22,6 +24,7 @@ export const products = [
     rating: 4.9
   },
   {
+    url:"https://www.aliexpress.com/item/32934380825.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=d3b65f65-f031-4565-a6c7-588e88677453&algo_expid=d3b65f65-f031-4565-a6c7-588e88677453-30&btsid=0be3769015822034557511194e6668&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/HTB1S4TWXe6sK1RjSsrbq6xbDXXab/choucong-Vintage-Court-Ring-925-sterling-Silver-Princess-cut-AAAAA-cz-stone-Engagement-Wedding-band-Rings.jpg_220x220xz.jpg_.webp",
     name: 'choucong Vintage Ring',
     price: 14.31,
@@ -29,6 +32,7 @@ export const products = [
     rating: 4.8
   },
   {
+    url:"https://www.aliexpress.com/item/32990379793.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7&algo_expid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7-53&btsid=0be3743b15822029777281371e805e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/H4e23e18a8ae3405a8c95d7388a9c2efef/2020-new-luxury-round-925-sterling-silver-wedding-ring-set-for-women-lady-anniversary-gift-jewelry.jpg_220x220xz.jpg_.webp",
     name: 'Huitan moonso',
     price: 31.98,
@@ -36,6 +40,7 @@ export const products = [
     rating: 5.0
   },
   {
+    url:"https://www.aliexpress.com/item/32914548568.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cd106c58-3222-4cab-8b0a-94803c796118&algo_expid=cd106c58-3222-4cab-8b0a-94803c796118-30&btsid=0ab6fb8815822033871658926edfd4&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/H280acc5a2abc47ebb9c15d78b80344c0w/JPalace-Heart-Red-Murano-Glass-Ring-925-Sterling-Silver-Rings-for-Women-Stackable-Ring-Band-Silver.jpg_220x220xz.jpg_.webp",
     name: 'JPalace Heart Ring',
     price: 9.99,
@@ -43,6 +48,7 @@ export const products = [
     rating: 4.8
   },
   {
+    url:"https://www.aliexpress.com/item/32849023403.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7&algo_expid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7-55&btsid=0be3743b15822029777281371e805e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/HTB1nx__lljTBKNjSZFDq6zVgVXa2/CWWZircons-Luxury-Wide-Circle-Women-Wedding-Rings-AAA-Cubic-Zirconia-Fashion-Geometric-Round-CZ-Bridal-Ring.jpg_220x220xz.jpg_.webp",
     name: 'CWWZircons Luxury',
     price: 6.86,
@@ -50,6 +56,7 @@ export const products = [
     rating: 4.8
   },
   {
+    url:"https://www.aliexpress.com/item/32952551589.html?spm=a2g0o.productlist.0.0.60ee2ec7XDHrl2&algo_pvid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c&algo_expid=e67afa11-6fd6-47d7-b8d8-cb0090aa117c-11&btsid=0be3764315817010946445542e8e5c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/HTB1vH8xOMDqK1RjSZSyq6yxEVXaL/Huitan-2PC-Bridal-font-b-Ring-b-font-with-Round-Brilliant-Cubic-Zircon-Prong-Setting-Anniversary.jpg_220x220xz.jpg_.webp",
     name: 'Huitan 2PC Bridal Ring',
     price: 1.48,
@@ -57,6 +64,7 @@ export const products = [
     rating: 4.5
   },
   {
+    url:"https://www.aliexpress.com/item/32825420051.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7&algo_expid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7-50&btsid=0be3743b15822029777281371e805e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
     imgUrl:"https://ae01.alicdn.com/kf/H30a965dea5a54c4a9d0359f5a4c332ef1/Modian-New-Style-Classic-Real-925-Sterling-silver-Square-Ring-Fashion-Wedding-Luxury-jewelry-Sparkling-CZ.jpg_220x220xz.jpg_.webp",
     name: 'Modian Classic Real',
     price: 10.01,
@@ -64,7 +72,8 @@ export const products = [
     rating: 4.7
   },
   {
-     imgUrl:"https://ae01.alicdn.com/kf/HTB1iTbKXH1YBuNjSszhq6AUsFXat/PATAYA-New-Arrivals-585-Rose-Gold-Double-Row-Micro-wax-Inlay-Natural-Zircon-Hollow-Rings-Women.jpg_220x220xz.jpg_.webp",
+    url:"https://www.aliexpress.com/item/32854535153.html?spm=a2g0o.productlist.0.0.60ee2ec7DmX2fS&algo_pvid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7&algo_expid=cecc78dc-d356-4be7-80aa-5a0c1ea3cff7-57&btsid=0be3743b15822029777281371e805e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
+    imgUrl:"https://ae01.alicdn.com/kf/HTB1iTbKXH1YBuNjSszhq6AUsFXat/PATAYA-New-Arrivals-585-Rose-Gold-Double-Row-Micro-wax-Inlay-Natural-Zircon-Hollow-Rings-Women.jpg_220x220xz.jpg_.webp",
     name: 'PATAYA New Arrivals',
     price: 11.31,
     description: '585 Rose Gold Zircon',
